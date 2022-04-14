b6da3f6c43bc9b97e6c15f832c52b2a9ae08bf41
Andrzej Kończyk <andrzej.konczyk@gmail.com>
2022-04-14 23:50:56 +0200

    nookmakers tables initial data finished

--- buckmachers analysis tables.sql
+++ buckmachers analysis tables.sql
@@ -38,7 +38,7 @@ round(100/avg_bsh, 2) as prct_bsh, round(100/avg_bsd, 2) as prct_bsd, round(100/
 from result_bookm
 
 -------
-/*analysis for b365 bookmaker */
+/*analysis for bet365 bookmaker */
 
 create view v_b365_ as
 select season, home_team, away_team, result, prct_b365h, prct_b365d, prct_b365a, 100-(prct_b365h + prct_b365d +prct_b365a) as uncertainty
@@ -173,16 +173,16 @@ order by prct_iwh desc)
 select * from overall_interwetten_predicted
 
 
-/* lasbet analysis */
+/* ladbrokers analysis */
 
 /*lbd = ibd and lba = iba */
 
-create view v_lasbet_ as
+create view v_ladbrokers_ as
 select season, home_team, away_team, result, prct_lbh, prct_ibd, prct_iba, 100-(prct_lbh + prct_ibd +prct_iba) as uncertainty
 from bookm_prctg
 
 
-create view v_lasbet_match_ as
+create view v_ladbrokers_match_ as
 select season, 
 sum(match) * 100 / count(match) as prct_match_b365
 from
@@ -197,11 +197,11 @@ when prct_ibd > (prct_lbh + prct_iba) then 'draw'
 when prct_iba > (prct_lbh + prct_ibd) then 'away win'
 else 'no prediction results'
 end as prediction
-from v_lasbet_ ))
+from v_ladbrokers_ ))
 group by season
 
 
-create table overall_lasbet_predicted as
+create table overall_ladbrokers_predicted as
 select distinct
 result, prct_lbh, prct_ibd, prct_iba, uncertainty, prediction
 from
@@ -211,10 +211,270 @@ when prct_ibd > (prct_lbh + prct_iba) then 'draw'
 when prct_iba > (prct_lbh + prct_ibd) then 'away win'
 else 'no prediction results'
 end as prediction
-from v_lasbet_ 
+from v_ladbrokers_ 
 where result = prediction
 order by prct_lbh desc)
 
+
+select * from overall_ladbrokers_predicted
+
+
+/*analysis for pinnacle bookmaker */
+
+create view v_pinnacle_ as
+select season, home_team, away_team, result, prct_psh, prct_psd, prct_psa, 100-(prct_psh + prct_psd +prct_psa) as uncertainty
+from bookm_prctg
+
+create view v_pinnacle_match_ as
+select season, 
+sum(match) * 100 / count(match) as prct_match_pinnacle
+from
+(select *,
+case when result = prediction then 1
+else 0
+end as match
+from
+(select *,
+case when prct_psh > (prct_psd + prct_psa) then 'home win'
+when prct_psd > (prct_psh + prct_psa) then 'draw'
+when prct_psa > (prct_psh + prct_psd) then 'away win'
+else 'no prediction results'
+end as prediction
+from v_pinnacle_ ))
+group by season
+
+
+create table overall_pinnacle_predicted as
+select distinct
+result, prct_psh, prct_psd, prct_psa, uncertainty, prediction
+from
+(select *,
+case when prct_psh > (prct_psd + prct_psa) then 'home win'
+when prct_psd > (prct_psh + prct_psa) then 'draw'
+when prct_psa > (prct_psh + prct_psd) then 'away win'
+else 'no prediction results'
+end as prediction
+from v_pinnacle_ 
+where result = prediction
+order by prct_psh desc)
+
+
+select * from overall_pinnacle_predicted
+
+
+/*analysis for William Hill bookmaker */
+
+/* wwh = whh */
+
+create view v_williamhill_ as
+select season, home_team, away_team, result, prct_wwh, prct_whd, prct_wha, 100-(prct_wwh + prct_whd +prct_wha) as uncertainty
+from bookm_prctg
+
+create view v_williamhill_match_ as
+select season, 
+sum(match) * 100 / count(match) as prct_match_williamhill
+from
+(select *,
+case when result = prediction then 1
+else 0
+end as match
+from
+(select *,
+case when prct_wwh > (prct_whd + prct_wha) then 'home win'
+when prct_whd > (prct_wwh + prct_wha) then 'draw'
+when prct_wha > (prct_wwh + prct_whd) then 'away win'
+else 'no prediction results'
+end as prediction
+from v_williamhill_ ))
+group by season
+
+
+create table overall_willaimhill_predicted as
+select distinct
+result, prct_wwh, prct_whd, prct_wha, uncertainty, prediction
+from
+(select *,
+case when prct_wwh > (prct_whd + prct_wha) then 'home win'
+when prct_whd > (prct_wwh + prct_wha) then 'draw'
+when prct_wha > (prct_wwh + prct_whd) then 'away win'
+else 'no prediction results'
+end as prediction
+from v_williamhill_ 
+where result = prediction
+order by prct_wwh desc)
+
+
+select * from overall_willaimhill_predicted
+
+/*analysis for Stan James bookmaker */
+
+create view v_stanjames_ as
+select season, home_team, away_team, result, prct_sjh, prct_sjd, prct_sja, 100-(prct_sjh + prct_sjd +prct_sja) as uncertainty
+from bookm_prctg
+
+create view v_stanjames_match_ as
+select season, 
+sum(match) * 100 / count(match) as prct_match_stanjames
+from
+(select *,
+case when result = prediction then 1
+else 0
+end as match
+from
+(select *,
+case when prct_sjh > (prct_sjd + prct_sja) then 'home win'
+when prct_sjd > (prct_sjh + prct_sja) then 'draw'
+when prct_sja > (prct_sjh + prct_sjd) then 'away win'
+else 'no prediction results'
+end as prediction
+from v_stanjames_ ))
+group by season
+
+
+create table overall_stanjames_predicted as
+select distinct
+result, prct_sjh, prct_sjd, prct_sja, uncertainty, prediction
+from
+(select *,
+case when prct_sjh > (prct_sjd + prct_sja) then 'home win'
+when prct_sjd > (prct_sjh + prct_sja) then 'draw'
+when prct_sja > (prct_sjh + prct_sjd) then 'away win'
+else 'no prediction results'
+end as prediction
+from v_stanjames_ 
+where result = prediction
+order by prct_sjh desc)
+
+
+select * from overall_stanjames_predicted
+
+/*analysis for vcbet bookmaker */
+
+create view v_vcbet_ as
+select season, home_team, away_team, result, prct_vch, prct_vcd, prct_vca, 100-(prct_vch + prct_vcd +prct_vca) as uncertainty
+from bookm_prctg
+
+create view v_vcbet_match_ as
+select season, 
+sum(match) * 100 / count(match) as prct_match_vcbet
+from
+(select *,
+case when result = prediction then 1
+else 0
+end as match
+from
+(select *,
+case when prct_vch > (prct_vcd + prct_vca) then 'home win'
+when prct_vcd > (prct_vch + prct_vca) then 'draw'
+when prct_vca > (prct_vch + prct_vcd) then 'away win'
+else 'no prediction results'
+end as prediction
+from v_vcbet_ ))
+group by season
+
+
+create table overall_vcbet_predicted as
+select distinct
+result, prct_vch, prct_vcd, prct_vca, uncertainty, prediction
+from
+(select *,
+case when prct_vch > (prct_vcd + prct_vca) then 'home win'
+when prct_vcd > (prct_vch + prct_vca) then 'draw'
+when prct_vca > (prct_vch + prct_vcd) then 'away win'
+else 'no prediction results'
+end as prediction
+from v_vcbet_ 
+where result = prediction
+order by prct_vch desc)
+
+
+select * from overall_vcbet_predicted
+
+
+/*analysis for Gamebookers bookmaker */
+
+create view v_gamebookers_ as
+select season, home_team, away_team, result, prct_gbh, prct_gbd, prct_gba, 100-(prct_gbh + prct_gbd +prct_gba) as uncertainty
+from bookm_prctg
+
+create view v_gamebookers_match_ as
+select season, 
+sum(match) * 100 / count(match) as prct_match_gamebookers
+from
+(select *,
+case when result = prediction then 1
+else 0
+end as match
+from
+(select *,
+case when prct_gbh > (prct_gbd + prct_gba) then 'home win'
+when prct_gbd > (prct_gbh + prct_gba) then 'draw'
+when prct_gba > (prct_gbh + prct_gbd) then 'away win'
+else 'no prediction results'
+end as prediction
+from v_gamebookers_ ))
+group by season
+
+
+create table overall_gamebookers_predicted as
+select distinct
+result, prct_gbh, prct_gbd, prct_gba, uncertainty, prediction
+from
+(select *,
+case when prct_gbh > (prct_gbd + prct_gba) then 'home win'
+when prct_gbd > (prct_gbh + prct_gba) then 'draw'
+when prct_gba > (prct_gbh + prct_gbd) then 'away win'
+else 'no prediction results'
+end as prediction
+from v_gamebookers_ 
+where result = prediction
+order by prct_gbh desc)
+
+
+select * from overall_gamebookers_predicted
+
+
+/*analysis for bluesquare bookmaker */
+
+create view v_bluesquare_ as
+select season, home_team, away_team, result, prct_bsh, prct_bsd, prct_bsa, 100-(prct_bsh + prct_bsd +prct_bsa) as uncertainty
+from bookm_prctg
+
+create view v_bluesquare_match_ as
+select season, 
+sum(match) * 100 / count(match) as prct_match_bluesquare
+from
+(select *,
+case when result = prediction then 1
+else 0
+end as match
+from
+(select *,
+case when prct_bsh > (prct_bsd + prct_bsa) then 'home win'
+when prct_bsd > (prct_bsh + prct_bsa) then 'draw'
+when prct_bsa > (prct_bsh + prct_bsd) then 'away win'
+else 'no prediction results'
+end as prediction
+from v_bluesquare_ ))
+group by season
+
+
+create table overall_bluesquare_predicted as
+select distinct
+result, prct_bsh, prct_bsd, prct_bsa, uncertainty, prediction
+from
+(select *,
+case when prct_bsh > (prct_bsd + prct_bsa) then 'home win'
+when prct_bsd > (prct_bsh + prct_bsa) then 'draw'
+when prct_bsa > (prct_bsh + prct_bsd) then 'away win'
+else 'no prediction results'
+end as prediction
+from v_bluesquare_ 
+where result = prediction
+order by prct_bsh desc)
+
+
+select * from overall_bluesquare_predicted
 
 
 -----
@@ -251,7 +511,62 @@ when prct_ibd > (prct_lbh + prct_iba) then 'draw'
 when prct_iba > (prct_lbh + prct_ibd) then 'away win'
 else 'no prediction results'
 end as prediction
-from v_lasbet_ 
+from v_ladbrokers_ 
+where result = prediction
+
+select *,
+case when prct_psh > (prct_psd + prct_psa) then 'home win'
+when prct_psd > (prct_psh + prct_psa) then 'draw'
+when prct_psa > (prct_psh + prct_psd) then 'away win'
+else 'no prediction results'
+end as prediction
+from v_pinnacle_ 
+where result = prediction
+
+select *,
+case when prct_wwh > (prct_whd + prct_wha) then 'home win'
+when prct_whd > (prct_wwh + prct_wha) then 'draw'
+when prct_wha > (prct_wwh + prct_whd) then 'away win'
+else 'no prediction results'
+end as prediction
+from v_williamhill_ 
+where result = prediction
+
+select *,
+case when prct_sjh > (prct_sjd + prct_sja) then 'home win'
+when prct_sjd > (prct_sjh + prct_sja) then 'draw'
+when prct_sja > (prct_sjh + prct_sjd) then 'away win'
+else 'no prediction results'
+end as prediction
+from v_stanjames_ 
+where result = prediction
+
+
+select *,
+case when prct_vch > (prct_vcd + prct_vca) then 'home win'
+when prct_vcd > (prct_vch + prct_vca) then 'draw'
+when prct_vca > (prct_vch + prct_vcd) then 'away win'
+else 'no prediction results'
+end as prediction
+from v_vcbet_ 
+where result = prediction
+
+select *,
+case when prct_gbh > (prct_gbd + prct_gba) then 'home win'
+when prct_gbd > (prct_gbh + prct_gba) then 'draw'
+when prct_gba > (prct_gbh + prct_gbd) then 'away win'
+else 'no prediction results'
+end as prediction
+from v_gamebookers_ 
+where result = prediction
+
+select *,
+case when prct_bsh > (prct_bsd + prct_bsa) then 'home win'
+when prct_bsd > (prct_bsh + prct_bsa) then 'draw'
+when prct_bsa > (prct_bsh + prct_bsd) then 'away win'
+else 'no prediction results'
+end as prediction
+from v_bluesquare_ 
 where result = prediction
 
 
